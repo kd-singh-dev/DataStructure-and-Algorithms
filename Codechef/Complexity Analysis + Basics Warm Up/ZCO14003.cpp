e58e54436ba5e6d01f26933ded7dd272ef495a3e
KarandeepSinghCoder <karandchandi@gmail.com>
2020-11-17 15:10:00 +0530

    ZCO14003

---
+++ Codechef/Complexity Analysis + Basics Warm Up/ZCO14003.cpp
@@ -0,0 +1,48 @@
+/*
+You are developing a smartphone app. You have a list of potential customers for your app. Each customer has a budget and will buy the app at your declared price if and only if the price is less than or equal to the customer's budget.
+
+
+You want to fix a price so that the revenue you earn from the app is maximized. Find this maximum possible revenue.
+
+
+For instance, suppose you have 4 potential customers and their budgets are 30, 20, 53 and 14. In this case, the maximum revenue you can get is 60 .
+
+
+Input format
+Line 1 : N, the total number of potential customers.
+
+Lines 2 to N+1: Each line has the budget of a potential customer.
+
+
+Output format
+The output consists of a single integer, the maximum possible revenue you can earn from selling your app.
+
+
+Sample Input 1
+4
+30
+20
+53
+14
+
+Sample Output 1
+60
+*/
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	long long int n;
+	cin>>n;
+	long long int a[n];
+	for(long long int i=0;i<n;i++)
+	    cin>>a[i];
+    sort(a, a + n);
+	long long int rev=0;
+	for(long long int i=0;i<n;i++)
+        rev=max(rev,a[i]*(n-i));
+	cout<<rev;
+	return 0;
+ }
